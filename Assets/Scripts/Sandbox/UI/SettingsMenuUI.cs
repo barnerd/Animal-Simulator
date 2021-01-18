86ddd6f9eed6eefc2e4dc29f120750da6d003f90
David Barner <dibarner@gmail.com>
2021-01-18 12:30:35 -0800

    UI Background Transparency
    Player can now control the Background transparency of the UI via slider in settings

--- Assets/Scripts/Sandbox/UI/SettingsMenuUI.cs
+++ Assets/Scripts/Sandbox/UI/SettingsMenuUI.cs
@@ -8,7 +8,11 @@ public class SettingsMenuUI : MonoBehaviour
     public GameObject settingsPanel;
     public GameObject creditsPanel;
 
+    [Header("UI Background Transparency")]
+    public FloatReference UIBackgroundTransparencyValue;
     public Text UIBackgroundTransparencyText;
+    public Slider UIBackgroundTransparencySlider;
+    public GameEvent onUIBackgroundTransparencyChange;
 
     public void SetTabPanelActive(GameObject activePanel)
     {
@@ -16,5 +20,16 @@ public class SettingsMenuUI : MonoBehaviour
         creditsPanel.SetActive(false);
 
         activePanel.SetActive(true);
+    }
+
+    public void OnUIBackgroundTransparencyChange()
+    {
+        UIBackgroundTransparencyValue.Value = UIBackgroundTransparencySlider.value;
+        onUIBackgroundTransparencyChange.Raise();
+    }
+
+    public void UpdateTransparencyText()
+    {
+        UIBackgroundTransparencyText.text = (UIBackgroundTransparencyValue.Value * 100).ToString("n0") + "%";
     }
 }
---
+++ Assets/Scripts/Sandbox/UI/UI.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    public Image UIPanelBackground;
+    public FloatReference UIBackgroundTransparencyValue;
+
+    // called via GameEvent onUIBackgroundTransparencyChange
+    public void UpdateBackgroundTransparency()
+    {
+        var tempColor = UIPanelBackground.color;
+        tempColor.a = UIBackgroundTransparencyValue.Value;
+        UIPanelBackground.color = tempColor;
+    }
+}
